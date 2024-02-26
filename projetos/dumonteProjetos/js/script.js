335125e93d02273a286e2a689d704d1863b1a9c1
Leandro Sani <leandrocs.sani@gmail.com>
2024-02-26 14:47:08 -0300

    atualizando dumonteProjetos

--- projetos/dumonteProjetos/js/script.js
+++ projetos/dumonteProjetos/js/script.js
@@ -38,3 +38,46 @@ setInterval(function (){
 
 }, 12000);
 
+/******************************click scroll******************************/
+/*
+let section1 = document.querySelector('#section1')
+let section2 = document.querySelector('#section2')
+let section3 = document.querySelector('#section3')
+*/
+
+let servicos = document.querySelector('.servicos');
+let projetos = document.querySelector('.projetos');
+let sobre = document.querySelector('.sobre');
+let call = document.querySelector('.call');
+let contato = document.querySelector('.contato');
+
+
+servicos.addEventListener("click",()=>{
+    window.scrollTo(0, 875);
+});
+projetos.addEventListener("click",()=>{
+    window.scrollTo(0, 1259);
+});
+sobre.addEventListener("click",()=>{
+    window.scrollTo(0, 1901);
+});
+call.addEventListener("click",()=>{
+    window.scrollTo(0, 875);
+});
+contato.addEventListener("click",()=>{
+    window.scrollTo(0, 2656);
+});
+
+/*
+window.addEventListener('scroll', function(ev){
+    console.log(this.scrollY);
+});
+*/
+
+// 875 section1
+
+// 1259 section2
+
+// 1901 section3
+
+// 2656 contato
